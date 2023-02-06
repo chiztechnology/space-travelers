b57fa1f1d31110e97c17223d89694ba2c3f58e12
Rashed Arman <rashedarman21@gmail.com>
2023-02-07 02:43:51 +0600

    Add font Poppins

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
