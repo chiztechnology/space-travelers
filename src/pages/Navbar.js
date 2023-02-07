42af58aac6673f47384554ecc6a962bb4ed8527a
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-02-07 22:08:39 +0200

    Set active navLink using internal props

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,21 +1,48 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar = () => {
-  const [activeMenu, setActiveMenu] = useState('rockets');
-  return (
-    <div className="navbar-container">
-      <div className="navbar-left-align">
-        <h1 className="logo"><Link className="navbar-logo-link" to="/">Space Travelers hub</Link></h1>
-        <ul className="navbar-ul">
-          <li className="navbar-ul-li"><Link className={`navbar-li-item ${activeMenu === 'rockets' ? 'active' : ''}`} to="/rockets" onClick={() => setActiveMenu('rockets')}>Rockets</Link></li>
-          <li className="navbar-ul-li"><Link className={`navbar-li-item ${activeMenu === 'missions' ? 'active' : ''}`} to="/missions" onClick={() => setActiveMenu('missions')}>Mission</Link></li>
-          <li className="navbar-ul-li"><Link className={`navbar-li-item ${activeMenu === 'profile' ? 'active' : ''}`} to="/profile" onClick={() => setActiveMenu('profile')}>Profile</Link></li>
-        </ul>
-      </div>
+const Navbar = () => (
+  <div className="navbar-container">
+    <div className="navbar-left-align">
+      <h1 className="logo"><NavLink className="navbar-logo-link" to="/">Space Travelers hub</NavLink></h1>
+      <ul className="navbar-ul">
+        <li className="navbar-ul-li">
+          <NavLink to="/rockets">
+            {({ isActive }) => (
+              <span
+                className={isActive ? 'navbar-li-item active' : 'navbar-li-item'}
+              >
+                Rockets
+              </span>
+            )}
+          </NavLink>
+        </li>
+        <li className="navbar-ul-li">
+          <NavLink to="/missions">
+            {({ isActive }) => (
+              <span
+                className={isActive ? 'navbar-li-item active' : 'navbar-li-item'}
+              >
+                Mission
+              </span>
+            )}
+          </NavLink>
+        </li>
+        <li className="navbar-ul-li">
+          <NavLink to="/profile">
+            {({ isActive }) => (
+              <span
+                className={isActive ? 'navbar-li-item active' : 'navbar-li-item'}
+              >
+                Profile
+              </span>
+            )}
+          </NavLink>
+        </li>
+      </ul>
     </div>
-  );
-};
+  </div>
+);
 
 export default Navbar;
