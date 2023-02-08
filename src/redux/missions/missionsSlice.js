d69cca6c59e5b5eac9c261a4c34306c39ea18631
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-02-08 22:00:24 +0200

    Fetch mission and create dispatch action

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,10 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import api from '../../api';
 
-const initialState = [];
+const FETCH = 'missions/FETCH';
+
+const initialState = {
+  missions: [],
+};
+
+export const getMissions = createAsyncThunk(FETCH, async () => {
+  const missions = await api.get('/missions');
+  return missions.data.map((mission) => ({
+    id: mission.mission_id,
+    name: mission.mission_name,
+    description: mission.description,
+  }));
+});
 
 const missionsSlice = createSlice({
   name: 'missions',
   initialState,
+  extraReducers: {
+    [getMissions.fulfilled]: (state, action) => {
+      const currState = state;
+      currState.missions = action.payload;
+    },
+  },
 });
 
 export default missionsSlice.reducer;
