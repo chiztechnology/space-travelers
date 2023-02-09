4c997a66851bb0e2ef4273e25b52fa22eede6d06
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-02-09 23:37:26 +0200

    Display reserved missions in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,9 +3,23 @@ import { useSelector } from 'react-redux';
 
 function ProfilePage() {
   const { rockets } = useSelector((state) => state.rockets);
+  const { missions } = useSelector((state) => state.missions);
 
   return (
-    <Container fluid="xl" className="mt-3">
+    <Container fluid="xl" className="mt-3" style={{ display: 'flex' }}>
+      <div className="w-50">
+        <h3>My Missions</h3>
+        <ListGroup>
+          {missions
+            .filter((mission) => mission.reserved)
+            .map(({ id, name }) => (
+              <ListGroup.Item className="p-4" key={id}>
+                {name}
+              </ListGroup.Item>
+            ))}
+        </ListGroup>
+      </div>
+
       <div className="w-50">
         <h3>My Rockets</h3>
         <ListGroup>
